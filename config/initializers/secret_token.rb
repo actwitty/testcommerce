9f6d09aa91cf9904ee86a75c84ad919a62b8afa4
Sudhanshu Saxena <sudhanshu@actwitty.com>
2012-12-19 15:55:28 +0530

    First commit on spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lemonbag::Application.config.secret_token = '0ca68f0fe7f101d30dc16b0c02339448386f1dd8e9326729f9666e853fb2ee97b8f160de4feacca8212daa96934930091af80e7da7f9b29d21c2bb629e64444a'
